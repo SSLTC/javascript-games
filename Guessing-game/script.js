04cc86865c7f3561ce827e61bb661a5d8dc0eae9
Snoeck Sven <snoeck.sven@live.be>
2023-01-18 11:47:28 +0100

    A style.css, a h1 header, an image with figcaption

---
+++ Guessing-game/script.js
@@ -0,0 +1,9 @@
+const elImg = document.createElement("img");
+elImg.setAttribute("src", "./images/pasteis.jpeg");
+elImg.setAttribute("alt", "Image of three pies");
+
+const elFigure = document.querySelector("figure");
+elFigure.insertBefore(elImg, elFigure.firstChild);
+
+const elFigCaption = document.querySelector("figcaption");
+elFigCaption.innerHTML = "Three pies whereof only one left, who will get it?"
