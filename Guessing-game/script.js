965441fc25aeb38f7b52a8f470fa147172bcb208
Snoeck Sven <snoeck.sven@live.be>
2023-01-18 15:59:25 +0100

    A working random number guess game with separate messages for right, wrong and close guess

--- Guessing-game/script.js
+++ Guessing-game/script.js
@@ -1,3 +1,5 @@
+import { getRndInteger } from "../modules/getRndInteger.js";
+
 const elImg = document.createElement("img");
 elImg.setAttribute("src", "./images/pasteis.jpeg");
 elImg.setAttribute("alt", "Image of three pies");
@@ -8,3 +10,30 @@ elFigure.insertBefore(elImg, elFigure.firstChild);
 const elFigCaption = document.querySelector("figcaption");
 elFigCaption.innerHTML = "Three pies whereof only one left, who will get it?"
 
+const randomNumber = 3 //getRndInteger(1, 30);
+
+const elResult = document.querySelector("#result");
+elResult.addEventListener("click", () => {
+    if(elResult.innerHTML !== "PLAY") { return };
+
+    const guess = elNumberInput.value;
+
+    if(guess == randomNumber) {
+        elResult.innerHTML = "Congratulations! <br/> You guessed right!";  
+    } else if (guess == randomNumber -1 || guess == randomNumber +1) {
+        elResult.innerHTML = `So close...<br/> The number was ${randomNumber}`;
+    } else {
+        elResult.innerHTML = `You guessed wrong<br/> The number was ${randomNumber}`;  
+    }
+
+    elNumberInput.value = null;
+    elNumberInput.setAttribute("disabled", false);
+});
+elResult.style.visibility = "hidden";
+
+const elNumberInput = document.querySelector("#guess");
+elNumberInput.addEventListener("change", (e) => {
+    if(elNumberInput.value != null) {
+        elResult.style.visibility = "visible";
+    }
+});
--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 ### Guessing-game
 
-- 
+- Let you only guess once the random generated number
+- Separate messages for right, wrong or close guess
 
 <a href="./Guessing-game">Guessing-game Link</a>
 
