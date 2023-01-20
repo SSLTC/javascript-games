0b519d01c4e2cedffa0694a78c55a7dbbdc34d42
Snoeck Sven <snoeck.sven@live.be>
2023-01-20 11:44:47 +0100

    Fix previous commit

--- 21-card-game/script.js
+++ 21-card-game/script.js
@@ -1,9 +1,11 @@
-import { deckOfCards } from "./modules/deckOfCards.js";
-import { gameplay21cardGame } from "./modules/21Rules.js";
-import { getRndInteger } from "./modules/getRndInteger.js";
+import deckOfCards from "./modules/deckOfCards.js";
+import gameplay21cardGame from "./modules/21Rules.js";
+import getRndInteger from "./modules/getRndInteger.js";
 
 let randomSuit = getRndInteger(0, 3);
 let randomCardOfSuit = getRndInteger(0, 12);
 
 let randomDrawnCard = deckOfCards[randomSuit].card[randomCardOfSuit];
+let removedCard = deckOfCards[randomSuit].card.splice(randomCardOfSuit, 1);
 console.log(randomDrawnCard);
+console.log(removedCard);
