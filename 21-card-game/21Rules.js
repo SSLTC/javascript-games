e3e99c6db8ef436be9976f913d9582a5db23ae16
Snoeck Sven <snoeck.sven@live.be>
2023-01-20 10:29:52 +0100

    Add deckOfCards.js, 21Rules.js, basic styling and a h1

---
+++ 21-card-game/21Rules.js
@@ -0,0 +1,6 @@
+const gameplay21cardGame = [
+    {
+        Gameplay: "Each player should draw 2 cards. Each player should keep their cards hidden; once players have been drawn their cards, they can opt to take another. This is known as a hit; if you want to use the traditional lingo, you simply say hit to take another card. Under traditional 21 card game rules, there are no limits to how many cards you can take. Although the more you take, the higher the odds are of you going over 21. If you go over 21, you have busted and must declare this to the other players. If you don’t want to take any more cards, you say stick. Once every player is happy with their cards, they will show their hands. The player with 21 or the closest to it wins. While it’s rare, multiple players could have the same score, so it could end in a draw."
+    }
+];
+export default gameplay21cardGame;
---
+++ 21-card-game/script.js
@@ -0,0 +1,3 @@
+import { deckOfCards } from "./deckOfCards.js";
+import { gameplay21cardGame } from "./21Rules";
+
